d4775bfea8d523b3fd6bfffdaa94300f5d92cff9
Tiark Rompf <tiark.rompf@epfl.ch>
2013-02-19 22:57:58 +0100

    first shot at interpreter test

--- src/test/scala/lancet/interpreter/test5.scala
+++ src/test/scala/lancet/interpreter/test5.scala
@@ -28,6 +28,69 @@ class TestInterpreter5 extends FileDiffSuite {
     println(f(100))
   }
 
+  def test2 = withOutFileChecked(prefix+"slowpath2") {
+    val it = new Decompiler
+
+    abstract class Exp
+    case class IntVal(x: Int) extends Exp
+    case class DoubleVal(x: Double) extends Exp
+    case class StringVal(x: String) extends Exp
+    case class Plus(x: Exp, y: Exp) extends Exp
+    case class Minus(x: Exp, y: Exp) extends Exp
+    case class Var(x: String) extends Exp
+    
+    abstract class Stm
+    case class Assign(x: String, y: Exp) extends Stm
+    case class While(x: Exp, y: Stm) extends Stm
+    case class Print(x: Exp) extends Stm
+    case class Block(xs: List[Stm]) extends Stm
+
+    val env = new scala.collection.mutable.HashMap[String,Any]
+    def eval(e: Exp): Any = e match {
+      case IntVal(x) => x
+      case DoubleVal(x) => x
+      case StringVal(x) => x
+      case Plus(x,y) => (eval(x),eval(y)) match {
+        case (x: Int, y: Int) => x + y
+        case (x: Double, y: Double) => x + y
+        case (x: String, y: String) => x + y 
+        // todo widen
+      }
+      case Minus(x,y) => (eval(x),eval(y)) match {
+        case (x: Int, y: Int) => x + y
+        case (x: Double, y: Double) => x + y
+        case (x: String, y: String) => x + y 
+        // todo widen
+      }
+      case Var(x) => env(x)
+    }
+    def exec(s: Stm): Unit = s match {
+      case Assign(x,y) => env(x) = eval(y)
+      case While(x,y) => while (eval(x).asInstanceOf[Int] > 0) exec(y) // test int
+      case Print(x) => println(eval(x))
+      case Block(xs) => xs.foreach(exec(_)) // unroll
+    }
+
+
+    val p = Block(List(
+      //Assign("max", IntVal(100)),
+      Assign("x", IntVal(0)),
+      While(Minus(Var("max"), Var("x")), Block(List(
+        Print(Var("x")),
+        Assign("x", Plus(Var("x"), IntVal(1)))
+      )))
+    ))
+
+    val f = it.compile { (x:Int) => 
+      env("max") = x
+      exec(p)
+    }
+    println(f(100))
+  }
+
+
+
+
 
   class Decompiler extends BytecodeInterpreter_Opt {
 
@@ -72,7 +135,7 @@ class TestInterpreter5 extends FileDiffSuite {
 
       Console.println(fullName)
 
-      // check for known collection methods
+      // check for known methods
       fullName match {
         case "lancet.interpreter.TestInterpreter5$Decompiler.dropdead" => handle {
           case r::Nil => 
