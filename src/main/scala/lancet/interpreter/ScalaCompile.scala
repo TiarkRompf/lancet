514e5c6fb44f25e4cd99866c62c9e992e0ff96b8
Tiark Rompf <tiark.rompf@epfl.ch>
2013-03-14 09:05:36 +0100

    enable setting scala compiler from repl

--- src/main/scala/lancet/interpreter/ScalaCompile.scala
+++ src/main/scala/lancet/interpreter/ScalaCompile.scala
@@ -14,6 +14,12 @@ object ScalaCompile {
 
   var compiler: Global = _
   //var output: ByteArrayOutputStream = _ 
+  var external = false
+
+  def setCompiler(c: Global): Unit = {
+    compiler = c
+    external = true
+  }
 
   def setupCompiler() = {
     /*
@@ -55,32 +61,40 @@ object ScalaCompile {
     val compiler = this.compiler
     val run = new compiler.Run
 
-    val fileSystem = new VirtualDirectory("<vfs>", None)
-    compiler.settings.outputDirs.setSingleOutput(fileSystem)
-  //      compiler.genJVM.outputDir = fileSystem
+    if (external) {    
+      run.compileSources(List(new util.BatchSourceFile("<stdin>", source.toString)))
+      val loader = Thread.currentThread().getContextClassLoader
+      val cls: Class[_] = loader.loadClass(className)
+      val cons = cls.getConstructor(staticData.map(_._2):_*)
+      cons.newInstance(staticData.map(_._1):_*).asInstanceOf[A=>B]
+    } else {
+      val fileSystem = new VirtualDirectory("<vfs>", None)
+      compiler.settings.outputDirs.setSingleOutput(fileSystem)
+    //      compiler.genJVM.outputDir = fileSystem
 
-    val reporter = new ConsoleReporter(compiler.settings, null, new PrintWriter(System.out)) // create new reporter because System.out may have changed
-    compiler.reporter = reporter
+      val reporter = new ConsoleReporter(compiler.settings, null, new PrintWriter(System.out)) // create new reporter because System.out may have changed
+      compiler.reporter = reporter
 
-    run.compileSources(List(new util.BatchSourceFile("<stdin>", source.toString)))
+      run.compileSources(List(new util.BatchSourceFile("<stdin>", source.toString)))
 
-    reporter.printSummary()
+      reporter.printSummary()
 
-    if (!reporter.hasErrors)
-      println("compilation: ok")
-    else
-      println("compilation: had errors")
+      if (!reporter.hasErrors)
+        println("compilation: ok")
+      else
+        println("compilation: had errors")
 
-    reporter.reset
-    //output.reset
+      reporter.reset
+      //output.reset
 
-    val parent = this.getClass.getClassLoader
-    val loader = new AbstractFileClassLoader(fileSystem, this.getClass.getClassLoader)
+      val parent = this.getClass.getClassLoader
+      val loader = new AbstractFileClassLoader(fileSystem, this.getClass.getClassLoader)
 
-    val cls: Class[_] = loader.loadClass(className)
-    val cons = cls.getConstructor(staticData.map(_._2):_*)
-    
-    val obj: A=>B = cons.newInstance(staticData.map(_._1):_*).asInstanceOf[A=>B]
-    obj
+      val cls: Class[_] = loader.loadClass(className)
+      val cons = cls.getConstructor(staticData.map(_._2):_*)
+      
+      val obj: A=>B = cons.newInstance(staticData.map(_._1):_*).asInstanceOf[A=>B]
+      obj
+    }
   }
 }
