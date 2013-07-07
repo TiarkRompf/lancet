e7448449b31bf866c35c40189d3d35527b959bfd
Tiark Rompf <tiark.rompf@epfl.ch>
2013-07-07 19:26:05 +0200

    update license header

--- src/test/scala/lancet/interpreter/test3.scala
+++ src/test/scala/lancet/interpreter/test3.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) %YEARS% Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  * 
  * This program is free software: you can redistribute it and/or modify
