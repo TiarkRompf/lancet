dcf0c7a3755fc8d76c53a441e0ac1ae8906dcfaf
Tiark Rompf <tiark.rompf@epfl.ch>
2013-07-17 03:46:18 +0200

    fix delite interface

--- src/main/scala/generated/scala/DeliteInterfaces.scala
+++ src/main/scala/generated/scala/DeliteInterfaces.scala
@@ -11,7 +11,7 @@ abstract class DeliteOpMultiLoop[A] {
   var loopSize: Int
   def alloc: A
   def processRange(__act: A, start: Int, end: Int): A //init+process
-  def init(__act: A, idx: Int): A
+  def init(__act: A, idx: Int, isEmpty:Boolean): A
   def process(__act: A, idx: Int): Unit
   def combine(__act: A, rhs: A): Unit
   def postCombine(__act: A, rhs: A): Unit
