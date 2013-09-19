8aa677b06a84e7720ea50bb6bc750679e8984277
Tiark Rompf <tiark.rompf@epfl.ch>
2013-09-19 17:56:45 +0200

    slightly more courageous test

--- src/test/scala/lancet/analysis/test4.scala
+++ src/test/scala/lancet/analysis/test4.scala
@@ -1279,12 +1279,12 @@ Map(
         While(Less(Const(0), Ref("x")), Block(List(
           //Assign("z", Plus(Ref("z"), Ref("x"))),
           //Assign("z2", Plus(Ref("z2"), Plus(Times(Ref("x"),Const(3)), Const(5)))),
-          //If(Less(Ref("y"),Const(17)), 
-            //Block(List(
-              //Assign("y", Plus(Ref("y"), Const(1)))
-            //)),
-            //Block(Nil)
-          //),
+          If(Less(Ref("y"),Const(17)), 
+            Block(List(
+              Assign("y", Plus(Ref("y"), Const(1)))
+            )),
+            Block(Nil)
+          ),
           Assign("x", Plus(Ref("x"), Const(-1)))
         ))),
         Assign("r", Ref("x"))
